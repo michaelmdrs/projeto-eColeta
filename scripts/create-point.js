dc807bec3df1bf7baa7cb85cff5c15c1b54fab14
Michael <mackelldev@gmail.com>
2020-06-06 10:27:07 -0300

    Povoamente dos estados e cidade usando Javascript

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,7 +1,7 @@
 function populateUFs() {
     const ufSelect = document.querySelector('select[name=txtUf]')
 
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
     .then( res => res.json() ) /* função anonima com retorno simples */
     .then( states => {
 
@@ -18,14 +18,19 @@ populateUFs()
 function getCities(event) {
     const citySelect = document.querySelector('select[name=txtCity]')
 
-    console.log(event.target.value)
-
     const ufValue = event.target.value
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
-    fetch()
+    fetch(url)
+    .then(res => res.json() )
+    .then( cities => {
+        for(const city of cities ) {
+            citySelect.innerHTML += `<option value='${cities.id}'>${cities.nome}</option>`
+        }
 
+        citySelect.disabled = false
+    })
 }
 
 
